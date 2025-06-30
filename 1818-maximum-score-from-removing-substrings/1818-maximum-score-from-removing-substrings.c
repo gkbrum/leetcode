96c5917e5703322d5822d3c971718b914be1a656
Gabriel Brum <64230488+gkbrum@users.noreply.github.com>
2025-06-30 00:28:42 -0300

    Time: 31 ms (10.81%), Space: 18.6 MB (10.81%) - LeetHub

---
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.c
@@ -0,0 +1,68 @@
+typedef struct {
+    int top;
+    int base;
+    int limit;
+    char ch[100001];
+} stack_t;
+
+stack_t *reset() {
+    stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
+    
+    stack->top = 0;
+    stack->limit = 100001;
+    stack->base = 0;
+    return stack;
+}
+
+void push(stack_t *stack, char ch) {
+    if (stack->top != stack->limit) {
+        stack->ch[stack->top] = ch;
+        stack->top++;
+    }
+}
+
+char pop(stack_t *stack) {
+    if (stack->top != 0) {
+        stack->top--;
+        return stack->ch[stack->top + 1];
+    }
+    return '\0';
+}
+
+int removePair(char *s, char c1, char c2, int value, char *s2) {
+    stack_t *stack =  reset();
+    int points = 0;
+    int len = strlen(s);
+
+    for (int i = 0; i < len; i++) {
+        if (stack->top != 0 && stack->ch[stack->top - 1] == c1 && s[i] == c2) {
+            pop(stack); // remove a letra empilhada na it anterior
+            points += value;
+        } else {
+            push(stack, s[i]);
+        }
+    }
+    push(stack, '\0'); //\0 para funcionar o strcpy
+
+    strcpy(s2, stack->ch); //coloca o resultado no formato de string
+
+    free(stack);
+    return points;
+}
+
+int maximumGain(char *s, int x, int y) {
+    int points = 0;
+    int len = strlen(s);
+    char *s2 = (char *)malloc(sizeof(char) * len + 1);
+    
+    if (x > y) {
+        points += removePair(s, 'a', 'b', x, s2);
+        points += removePair(s2, 'b', 'a', y, s);
+    } else {
+        points += removePair(s, 'b', 'a', y, s2);
+        points += removePair(s2, 'a', 'b', x, s);
+    }
+
+    free(s2);
+    return points;
+}
