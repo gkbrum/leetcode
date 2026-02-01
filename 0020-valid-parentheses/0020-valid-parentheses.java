3cf16d3afb1986ce755266a77cbf45d552519613
Gabriel Brum <64230488+gkbrum@users.noreply.github.com>
2026-01-31 23:27:21 -0300

    Time: 4 ms (42.79%), Space: 43.6 MB (23.46%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(char c: s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                if(c == ')'){
+                    if(stack.isEmpty()) return false;
+                    if(stack.peek() == '('){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+                
+                if(c == ']'){
+                    if(stack.isEmpty()) return false;
+                    if(stack.peek() == '['){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+
+                if(c == '}'){
+                    if(stack.isEmpty()) return false;
+                    if(stack.peek() == '{'){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+
+        if (stack.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
